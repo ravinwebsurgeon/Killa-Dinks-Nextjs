ef734be207f540a77e1720037b12a3f7e0cccf5e
Deepak Verma <deepak@itsbold.com>
2024-11-27 14:00:08 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import { Carousel } from 'components/carousel';
-import { ThreeItemGrid } from 'components/grid/three-items';
-import Footer from 'components/layout/footer';
 
 export const metadata = {
   description: 'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
@@ -12,9 +9,7 @@ export const metadata = {
 export default function HomePage() {
   return (
     <>
-      <ThreeItemGrid />
-      <Carousel />
-      <Footer />
+      
     </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,13 @@
 export default {
+  env: {
+    COMPANY_NAME: "Vercel Inc.",
+    TWITTER_CREATOR: "@vercel",
+    TWITTER_SITE: "https://nextjs.org/commerce",
+    SITE_NAME: "Next.js Commerce",
+    SHOPIFY_REVALIDATION_SECRET: "",
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN: "c6464232b49e46e4c9d64d7af22fa38f",
+    SHOPIFY_STORE_DOMAIN: "dourdinks.myshopify.com"
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
