ce03c1bb99c80d9ededdc3c65447d1bd39b13a29
Simranpreet Singh <simranpreet@thefabcode.org>
2024-11-28 19:11:46 +0530

    Shop module items fixed

--- components/homepage/ShopBanner.tsx
+++ components/homepage/ShopBanner.tsx
@@ -6,23 +6,24 @@ const ShopBanner = ({ products }: any) => {
             <section className="shop-our-brand flex justify-center mx-auto px-4 sm:px-8">
                 <div className="w-full max-w-[1720px] h-auto bg-[#BBA887] rounded-[9px] lg:rounded-[50px]">
 
-                    <div className="text-white font-[500] text-[32px] sm:text-[40px] leading-[48px] sm:leading-[60px] flex justify-center pt-[40px] sm:pt-[80px]">
+                    <div className="text-white font-[500] text-[18px] md:text-[32px] sm:text-[40px] leading-[48px] sm:leading-[60px] flex justify-center pt-4 md:pt-[40px] sm:pt-[80px]">
                         Shop Our Brand
                     </div>
 
-                    <div className="flex flex-wrap justify-center gap-[36px] pt-[40px] pb-[50px] ">
+                    <div className="grid  gap-[36px] pt-[40px] place-items-center pb-[50px] mx-auto max-w-screen-xl" style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(340px, 1fr))' }}>
+
                         {
                             products && products.map((item: any, index: any) => {
                                 return (
                                     <div key={index}>
-                                        <div className="w-full sm:w-[340px] h-[430px] flex-col  overflow-hidden border-[1px] rounded-[20px] text-white  mx-[20px] lg:mx-0">
-                                            <div className="max-h-[302px]  overflow-hidden group bg-[#FAF7EB] h-full rounded-t-[20px] flex justify-center items-center">
+                                        <div className="w-full min-w-[340px] sm:w-[340px] min-h-[380px] h-auto flex-col  overflow-hidden border-[1px] rounded-[20px] text-white  lg:mx-0">
+                                            <div className="max-h-[302px] xl:h-[302px]  overflow-hidden group bg-[#FAF7EB] h-full rounded-t-[20px] flex justify-center items-center">
                                                 <img src={item.images[0].url} alt="" className="w-[250px] h-[250px] transition-transform duration-500 ease-in-out group-hover:scale-110" />
                                             </div>
-                                            <div className="pl-[18px]">
+                                            <div className=" flex flex-col gap-1 pl-[18px]">
                                                 <div className="pt-[10px] text-[16px] sm:text-[18px]">{item.title}</div>
                                                 <div className="text-[18px] sm:text-[20px]">${item.priceRange.maxVariantPrice.amount}{" "} USD</div>
-                                                <Link  href={`/product/${item.handle}`}  className="my-[10px] py-[6px] text-[16px] sm:text-[18px] text-[#BBA887] bg-[#FAF7EB] px-6 rounded-[20px]">
+                                                <Link  href={`/product/${item.handle}`}  className="my-[10px] py-[6px] max-w-[200px] text-[16px] sm:text-[18px] text-[#BBA887] bg-[#FAF7EB] px-6 rounded-[20px]">
                                                     Choose Options
                                                 </Link>
                                             </div>
