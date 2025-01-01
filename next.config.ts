fe565c3a9417013d2e3914bb553869c7da3bb605
Ravin <ravinwebsurgeon@gmail.com>
2025-01-01 17:52:50 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
+  env: {
+    COMPANY_NAME: "Vercel Inc.",
+    TWITTER_CREATOR: "@vercel",
+    TWITTER_SITE: "https://nextjs.org/commerce",
+    SITE_NAME: "Next.js Commerce",
+    SHOPIFY_REVALIDATION_SECRET: "",
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN: "c6464232b49e46e4c9d64d7af22fa38f",
+    SHOPIFY_STORE_DOMAIN: "dourdinks.myshopify.com",
+    JUDGME_API_TOKEN : "23j4V9skYoK0bviue3k-8SqNthA"
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     domains: ['cdn.sanity.io'], // Add the domain here
@@ -12,4 +21,3 @@ const nextConfig = {
     ]
   }
 };
-module.exports = nextConfig;
